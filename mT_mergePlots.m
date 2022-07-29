95641879a10dfe00c8033f9b89f79246f07e4853
jCalderTravis <38797399+jCalderTravis@users.noreply.github.com>
2022-07-29 11:08:27 +0200

    Improving merge plot options

--- mT_mergePlots.m
+++ mT_mergePlots.m
@@ -1,4 +1,4 @@
-function finalFig = mT_mergePlots(inFigs, mergeMode)
+function finalFig = mT_mergePlots(inFigs, mergeMode, varargin)
 % Copy all the data from a series of plots into a single figure. All
 % original figures will be closed.
 
@@ -8,14 +8,29 @@ function finalFig = mT_mergePlots(inFigs, mergeMode)
 %   "grid" for a grid of all the plots, one after another. Axis limits 
 %       may be changed.
 %   "spotlight" makes the first figure large and then puts the next three
-%       figures smaller, underneath this. inFigs must be 4 figures long. If
+%       figures smaller, above this. inFigs must be 4 figures long. If
 %       the input figures have more than a single axis, then this operation
 %       will be repeated for each axis. All figures must have the same
-%       number of axes. Removes all x- and y- labels except for the
-%       highlighted plot
+%       number of axes. The legend from the final axis in the first figure
+%       is copied without checking if this legend applies to any of the 
+%       other plots. If varargin{1} is true, some extraneous labels 
+%       and tick labels will be removed from the plots.
+% varargin{1}: bool. Default true. If true, check all input figures have 
+%   the same number of axes and that, the i'th axis across all figures
+%   has the same xlabel, ylabel, and ticks (for all i). 
 
 % OUTPUT
 % finalFig: Figure handle for the combined figure
+
+if (~isempty(varargin)) && (~isempty(varargin{1}))
+    checkEquiv = varargin{1};
+else
+    checkEquiv = true;
+end
+
+if checkEquiv
+    checkEquivalence(inFigs);
+end
 
 finalFig = figure;
 
@@ -40,7 +55,8 @@ elseif strcmp(mergeMode, 'spotlight')
     widthPerAx = 3;
     heightPerAx = 4;
     
-    tilePlt = tiledlayout(finalFig, 4, widthPerAx*numAxes);
+    tilePlt = tiledlayout(finalFig, 4, widthPerAx*numAxes, ...
+        'TileSpacing', 'compact');
     
     for iAx = 1 : numAxes
         for iF = 1 : length(inFigs)
@@ -54,21 +70,45 @@ elseif strcmp(mergeMode, 'spotlight')
             thisAx.Parent = tilePlt;
             
             if iF == 1
-                thisAx.Layout.Tile = 1 + ((iAx-1) * widthPerAx);
+                numTilesInFirstRow = widthPerAx*numAxes;
+                
+                thisAx.Layout.Tile = 1 + ((iAx-1) * widthPerAx) + ...
+                    numTilesInFirstRow;
                 thisAx.Layout.TileSpan = [heightPerAx-1, widthPerAx];
             else
-                numPreviousTiles = (heightPerAx-1)*(widthPerAx*numAxes);
-                numPreviousTiles = numPreviousTiles + ...
-                    (widthPerAx * (iAx - 1));
+                numPreviousTiles = widthPerAx * (iAx - 1);
                 
                 thisAx.Layout.Tile = numPreviousTiles + iF - 1;
                 thisAx.Layout.TileSpan = [1, 1];
             end
             
-            % Remove x- or y-labels?
-            if iF ~= 1
-                xlabel(thisAx, '')
-                ylabel(thisAx, '')
+            % Remove some labels?
+            if checkEquiv
+                if iF > 1
+                    xlabel(thisAx, '')
+                    ylabel(thisAx, '')
+                end
+                
+                if iF == 2
+                   thisAx.XTickLabels = ...
+                       retainOnlyEndElements(thisAx.XTickLabels);
+                   thisAx.YTickLabels = ...
+                       retainOnlyEndElements(thisAx.YTickLabels);
+                end
+                
+                if iF > 2
+                    thisAx.XTickLabels = ...
+                        cell(length(thisAx.XTickLabels), 1);
+                    thisAx.YTickLabels = ...
+                        cell(length(thisAx.YTickLabels), 1);
+                end
+            end
+            
+            % Legend
+            if (iF == 1) && (iAx == numAxes)
+                % WORKING HERE -- this not working
+               leg = findobj(inFigs(iF), 'type', 'Legend');
+               copyobj(leg, thisAx);
             end
         end
     end
@@ -105,6 +145,60 @@ end
     
 end
 
+function checkEquivalence(inFigs)
+
+if length(inFigs) == 1
+   warning(['Only one figure provided to merge function. Therefore, ', ...
+       'can not check equivalence accross figures.'])
+   return
+end
+
+numAxes = findNumAxes(inFigs);
+for iAx = 1 : numAxes
+    
+    allAx = findall(inFigs(1), 'type', 'axes');
+    firstFigAx = allAx(iAx);
+    
+    for iF = 2 : length(inFigs)
+        allAx = findall(inFigs(iF), 'type', 'axes');
+        thisAx = allAx(iAx);
+        
+        assert(isequal(firstFigAx.XLabel.String, thisAx.XLabel.String))
+        assert(isequal(firstFigAx.XLim, thisAx.XLim))
+        assert(isequal(firstFigAx.XTick, thisAx.XTick))
+        assert(isequal(firstFigAx.XTickLabel, thisAx.XTickLabel))
+        
+        assert(isequal(firstFigAx.YLabel.String, thisAx.YLabel.String))
+        assert(isequal(firstFigAx.YLim, thisAx.YLim))
+        assert(isequal(firstFigAx.YTick, thisAx.YTick))
+        assert(isequal(firstFigAx.YTickLabel, thisAx.YTickLabel))
+    end
+end
+
+end
+
+function cellOut = retainOnlyEndElements(cellVector)
+% For a cell array with the shape of a vector, return another cell array of
+% the same shape but that is empty, except for the non-empty entries in the 
+% input array nearest the start and end of the vector.
+
+nonZero = cellfun(@(el)~isEmptyOrEmptyStr(el), cellVector);
+nonZero = find(nonZero);
+
+cellOut = cell(size(cellVector));
+cellOut(nonZero(1)) = cellVector(nonZero(1));
+cellOut(nonZero(end)) = cellVector(nonZero(end));
+
+end
+
+function result = isEmptyOrEmptyStr(el)
+
+if isempty(el) || strcmp('', el) || strcmp(' ', el)
+    result = true;
+else
+    result = false;
+end
+end
 
 
 
